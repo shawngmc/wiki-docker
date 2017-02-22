46e619637fc3767a5d66cc0b92f8abdf6c23e846
shawngmc <shawngmc@gmail.com>
2017-02-21 19:11:32 -0500

    Create mongodb.sh

---
+++ mongodb.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -eo pipefail
+
+[[ -z $DATA_DIR ]] && DATA_DIR=/data/db
+
+[[ -d $DATA_DIR ]] || mkdir -p $DATA_DIR
+chown -R mongodb:mongodb $DATA_DIR
+chmod -R 700 $DATA_DIR
+
+exec /sbin/setuser mongodb mongod --config /etc/mongodb/mongodb.yml
